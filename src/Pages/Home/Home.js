ad1e9dbead708e4ee8995d156f9d475293cc2077
Vincent <makokhavomondi@gmail.com>
2023-02-27 18:26:30 +0300

    :sparkles: Create homepage directory

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Introduction from "../../Sections/Introduction/Introduction";
+
+function Home(){
+    return (
+        <div id="home" className="container">
+            <Introduction />
+        </div>
+    )
+}
+
+export default Home;
