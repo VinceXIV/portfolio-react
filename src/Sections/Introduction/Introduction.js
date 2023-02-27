6283de4998974af610174050497376eb5e7e58f3
Vincent <makokhavomondi@gmail.com>
2023-02-27 20:28:21 +0300

    :lipstick: Use the triangle element to decorate stuff in introduction section

--- src/Sections/Introduction/Introduction.js
+++ src/Sections/Introduction/Introduction.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Triangle from "../../Elements/Triangle/Triangle";
 import "./Introduction.css"
 
 function Introduction(){
@@ -7,7 +8,11 @@ function Introduction(){
             <h1>Hello there,</h1>
             <div class="name">
                 <div id="i-am-and-decorative-div">
-                    <p id="i-am">I am</p>
+                    <div id="i-am">
+                        <p>I</p>
+                        <Triangle width={12} height={16} color={"#C89595"} className=""/>
+                        <p>am</p>
+                    </div>
                     <div class="decoration-div"></div>
                 </div>
 
@@ -20,6 +25,7 @@ function Introduction(){
             <div id="welcome-to-my-world">
                 <p className="welcome-content"> Welcome to</p>
                 <p className="welcome-content">{`{ myWorld }`}</p>
+                <Triangle width={12} height={16} color={"#C89595"} className="welcome-content-triangle"/>
             </div>
         </section>
     )
