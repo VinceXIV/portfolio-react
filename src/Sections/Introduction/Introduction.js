d6a7aa6a059e29d1013b6e83454ee5e3671881dc
Vincent <makokhavomondi@gmail.com>
2023-02-28 15:12:45 +0300

    :bug: Fix a warning resulting from using "class" instead of "className"

--- src/Sections/Introduction/Introduction.js
+++ src/Sections/Introduction/Introduction.js
@@ -7,14 +7,14 @@ function Introduction(){
         <section id="section-1--introduction">
             <div className="left-side">
                 <h1>Hello there,</h1>
-                <div class="name">
+                <div className="name">
                     <div id="i-am-and-decorative-div">
                         <div id="i-am">
                             <p>I</p>
                             <Triangle width={0.75} height={0.75} color={"#C89595"} className=""/>
                             <p>am</p>
                         </div>
-                        <div class="decoration-div"></div>
+                        <div className="decoration-div"></div>
                     </div>
 
                     <div id="my-name">
