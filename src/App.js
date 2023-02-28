d2e1428612d444c71e062d56be860e289b9bd1e1
Vincent <makokhavomondi@gmail.com>
2023-02-28 19:16:09 +0300

    :lipstick: Active styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import React, {useState} from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home/Home';
 
 function App() {
+  const [activePage, setActivePage] = useState('home')
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar activePage={activePage} setActivePage={setActivePage} />
       <Home />
     </div>
   );
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Navbar.css"
 
-function Navbar(){
+function Navbar({activePage, setActivePage}){
     return (
         <nav id="navbar">
             <div className="container">
@@ -11,11 +11,31 @@ function Navbar(){
         
                 <div id="div-right">
                     <ul>
-                        <li><a href="#section-1--introduction">Introduction</a></li>
-                        <li><a href="#section-2--about-me">About Me</a></li>
-                        <li><a href="#section-3--skills">My Skills</a></li>
-                        <li><a href="#section-4--featured-projects">Projects</a></li>
-                        <li><a href="#section-5--get-in-touch">Contact</a></li>
+                        <li className={activePage === 'introduction'? 'active' : ''}
+                            onClick={()=>setActivePage('introduction')}
+                            >
+                            <a href="#section-1--introduction">Introduction</a>
+                        </li>
+                        <li className={activePage === 'about-me'? 'active' : ''}
+                            onClick={()=>setActivePage('about-me')}
+                            >
+                            <a href="#section-2--about-me">About Me</a>
+                        </li>
+                        <li className={activePage === 'my-skills'? 'active' : ''}
+                            onClick={()=>setActivePage('my-skills')}
+                            >
+                            <a href="#section-3--skills">My Skills</a>
+                        </li>
+                        <li className={activePage === 'projects'? 'active' : ''}
+                            onClick={()=>setActivePage('projects')}
+                            >
+                            <a href="#section-4--featured-projects">Projects</a>
+                        </li>
+                        <li className={activePage === 'contact'? 'active' : ''}
+                            onClick={()=>setActivePage('contact')}
+                            >
+                            <a href="#section-5--get-in-touch">Contact</a>
+                        </li>
                     </ul>
                 </div>
             </div>
