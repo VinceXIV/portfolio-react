d21ddcd3a8f25e9bec1b5cf0c8b1215523e11d6a
Vincent <makokhavomondi@gmail.com>
2023-03-08 19:23:29 +0300

    Revert "Use grids"
    This reverts commit 432b9b6a0d715b0a2f458d22fde6c15236df3571.

--- src/Elements/TwoSquares/TwoSquares.js
+++ src/Elements/TwoSquares/TwoSquares.js
@@ -1,42 +1,46 @@
 import React from "react";
-import "./TwoSquares.css"
 
 function TwoSquares({width=3, height=10, gap=0.5, borderColor="var(--primary-color-2)", overlap=3}){
-    const squareStyle = {
+    const square1Style = {
         width: `${width}rem`,
         height: `${height}rem`,
-    }
-    const square1Style = {
         borderBottom: `0.05rem solid ${borderColor}`,
         borderRight: `0.05rem solid ${borderColor}`,
     }
 
-    const square4Style = {
+    const square2Style = {
+        width: `${width}rem`,
+        height: `${height}rem`,
         borderTop: `0.05rem solid ${borderColor}`,
         borderLeft: `0.05rem solid ${borderColor}`,
     }
 
     const twoSquaresStyle = {
         height: `${(2*height) - overlap}rem`,
-        display: "grid",
+        display: "flex",
         gap: `${gap}rem`,
+        top: "10rem",
+        left: "10rem"
     }
 
-    const dummyStyle = {
-        height:`${height - (overlap/2)}rem`
+    const squareContainerStyle = {
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
     }
 
 
     return (
-        <div style={twoSquaresStyle} id='two-squares'>
-                <div id="square-1" style={{...square1Style, ...squareStyle}} className="square"></div>
-                <div id="square-2" className="square dummy" style={dummyStyle}></div>
-            {/* <div style={squareContainerStyle}>
-            </div> */}
-                <div id="square-3" className="square dummy" style={dummyStyle}></div>
-                <div id="square-4" style={{...square4Style, ...squareStyle}}></div>
-            {/* <div style={squareContainerStyle}>
-            </div> */}
+        <div style={twoSquaresStyle}>
+            <div style={squareContainerStyle}>
+                <div id="square-1" style={square1Style}></div>
+                <div></div>
+            </div>
+            <div style={squareContainerStyle}>
+                <div></div>
+                <div id="square-2" style={square2Style}></div>
+            </div>
         </div>
     )
 }
