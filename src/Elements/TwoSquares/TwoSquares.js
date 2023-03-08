432b9b6a0d715b0a2f458d22fde6c15236df3571
Vincent <makokhavomondi@gmail.com>
2023-03-08 19:23:20 +0300

    Use grids

--- src/Elements/TwoSquares/TwoSquares.js
+++ src/Elements/TwoSquares/TwoSquares.js
@@ -1,46 +1,42 @@
 import React from "react";
+import "./TwoSquares.css"
 
 function TwoSquares({width=3, height=10, gap=0.5, borderColor="var(--primary-color-2)", overlap=3}){
-    const square1Style = {
+    const squareStyle = {
         width: `${width}rem`,
         height: `${height}rem`,
+    }
+    const square1Style = {
         borderBottom: `0.05rem solid ${borderColor}`,
         borderRight: `0.05rem solid ${borderColor}`,
     }
 
-    const square2Style = {
-        width: `${width}rem`,
-        height: `${height}rem`,
+    const square4Style = {
         borderTop: `0.05rem solid ${borderColor}`,
         borderLeft: `0.05rem solid ${borderColor}`,
     }
 
     const twoSquaresStyle = {
         height: `${(2*height) - overlap}rem`,
-        display: "flex",
+        display: "grid",
         gap: `${gap}rem`,
-        top: "10rem",
-        left: "10rem"
     }
 
-    const squareContainerStyle = {
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
+    const dummyStyle = {
+        height:`${height - (overlap/2)}rem`
     }
 
 
     return (
-        <div style={twoSquaresStyle}>
-            <div style={squareContainerStyle}>
-                <div id="square-1" style={square1Style}></div>
-                <div></div>
-            </div>
-            <div style={squareContainerStyle}>
-                <div></div>
-                <div id="square-2" style={square2Style}></div>
-            </div>
+        <div style={twoSquaresStyle} id='two-squares'>
+                <div id="square-1" style={{...square1Style, ...squareStyle}} className="square"></div>
+                <div id="square-2" className="square dummy" style={dummyStyle}></div>
+            {/* <div style={squareContainerStyle}>
+            </div> */}
+                <div id="square-3" className="square dummy" style={dummyStyle}></div>
+                <div id="square-4" style={{...square4Style, ...squareStyle}}></div>
+            {/* <div style={squareContainerStyle}>
+            </div> */}
         </div>
     )
 }
