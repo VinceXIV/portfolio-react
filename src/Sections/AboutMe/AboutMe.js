2009e02cb245bfa3f77f492fb13494804c7433a7
Vincent <makokhavomondi@gmail.com>
2023-03-08 21:46:24 +0300

    Start working on about me

--- src/Sections/AboutMe/AboutMe.js
+++ src/Sections/AboutMe/AboutMe.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CharacterPanels from "../../Components/CharacterPanels/CharacterPanels";
+import TwoSquares from "../../Elements/TwoSquares/TwoSquares";
 import "./AboutMe.css"
 
 function AboutMe(){
@@ -7,6 +8,13 @@ function AboutMe(){
         <div id="about-me-section">
             <div id="about-me-decoration">
                 <CharacterPanels />
+                <div className="two-squares">
+                    <TwoSquares height={5} width={2} overlap={2}/>
+                    <TwoSquares height={5} width={2} overlap={2}/>
+                    <TwoSquares height={5} width={2} overlap={2}/>
+                    <TwoSquares height={5} width={2} overlap={2}/>
+                    <TwoSquares height={5} width={2} overlap={2}/>
+                </div>
             </div>
             <div id="about-me-content"></div>
         </div>
