d8370f87c0aafe81ddcb5d2910fdb013b8f6184b
Vincent <makokhavomondi@gmail.com>
2023-03-08 20:04:08 +0300

    :sparkles: Generate numberlist and character panels

---
+++ src/Components/CharacterPanels/CharacterPanels.js
@@ -0,0 +1,24 @@
+import React from "react";
+import CharacterList from "../../Elements/NumberList/CharacterList";
+import "./CharacterPanels.css"
+
+function CharacterPanels({panels=[21, 15, 9], values=[0, 1]}){
+
+    function handleOnClick(e){
+        e.target.style.visibility = 'hidden'
+    }
+
+    return (
+        <div id="character-panels">
+            {
+                panels.map((panel, index) => {
+                    return (
+                        <CharacterList key={index}  count={panel} values={values} handleOnClick={handleOnClick}/>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default CharacterPanels;
